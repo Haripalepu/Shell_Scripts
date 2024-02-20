3f64dd36d63e6ced3b25e53a2ef384547da99498
hari <hari@gmail.com>
2024-02-21 00:07:25 +0530

    mail

--- 18.disk_usage.sh
+++ 18.disk_usage.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-DISK_USAGE=$(df -hT | grep -vE 'tmp|File') #-V will exclude the tmp and file names and prints rest
+DISK_USAGE=$(df -hT | grep -vE 'tmp|File') #-v will exclude the tmp files system and file name and prints rest
 DISK_THRESHOLD=1
 message=""
 
 while IFS= read line
 do 
- usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
- partition=$(echo $line | awk '{print $1F}')
+ usage=$(echo $line | awk '{print $6}' | cut -d % -f1)  #$6 means disk % , run df -hT and count the line.
+ partition=$(echo $line | awk '{print $1F}') #Command to get the disk partition.
  if [ $usage -gt $DISK_THRESHOLD ]
   then 
      message+="High Disk Usage on $partition: $usage<br>"
@@ -20,3 +20,7 @@ echo -e "Message: $message"
 #echo "$message" | mail -s "High Disk Usage" crazyharee@gmail.com 
 
 sh 20.Mail.sh "Hi Team" "High Disk Usage" "$message" "crazyharee@gmail.com" "Alert Recived On Disk Utilization"
+
+
+#Monitoring team will monitor Servers, websites. if website is down then they will escalate to DevOps teams.
+#If server is down they will escalate to Linux team.
