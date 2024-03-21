69c1c6379071190441f1fb8857565e5e7d58691c
hari <hari@gmail.com>
2024-03-21 22:47:29 +0530

    Updating

--- 21.Roboshop.sh
+++ 21.Roboshop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-AMI=ami-081609eef2e3cc958 #Replace the ami id
+AMI=ami-0f3c7d07486cad139 #Replace the ami id
 SG_ID=sg-068f4d593bbb78b34
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-ZONE_ID=Z0259905UVRRPZ13D1QO #Replace with you zoneID
+ZONE_ID=Z08634832J9AMNTG9D2CW #Replace with you zoneID
 DOMAIN_NAME=haripalepu.cloud
 
 for i in "${INSTANCES[@]}"
@@ -16,7 +16,7 @@ do
       INSTANCE_TYPE="t2.micro"
   fi
 
-IP_ADDRESS=$(aws ec2 run-instances --image-id ami-081609eef2e3cc958 --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-068f4d593bbb78b34 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-068f4d593bbb78b34 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
 echo "$i: $IP_ADDRESS"    #Replace the ami id
 
 #create R53 record, make sure you delete existing record
